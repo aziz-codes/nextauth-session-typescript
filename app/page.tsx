0fd3666397013c0c05a5a9ee2390b12a75c6a262
aziz-codes <azizisoffline@gmail.com>
2024-09-10 22:44:58 +0500

    conditional rendering for session

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client"
 import React from 'react'
-
+import { useSession } from 'next-auth/react'
+import Image from 'next/image';
+import SignIn from './components/sign-in';
 const HomePage = () => {
-  console.log(process.env.CLIENT_ID)
+  const {data:session} = useSession(); 
   return (
-    <div>HomePage</div>
+    <div className='h-screen w-full flex justify-center items-center flex-col gap-4'>
+      {session ? <div>
+        <Image src={session?.user?.image as string} height={100} width={100} alt='avatar' className='border p-2 ring-1 border-sky-500 rounded-full'/>
+        <h4 className='font-semibold text-sm'>{session?.user?.name}</h4>
+      </div>: <SignIn />}
+    </div>
   )
 }
 
