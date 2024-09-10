5d7cc26eb70ea67c9ded9f8e3c092bd8f335d452
aziz-codes <azizisoffline@gmail.com>
2024-09-10 22:49:52 +0500

    loading state for session

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import Image from 'next/image';
 import SignIn from './components/sign-in';
 const HomePage = () => {
-  const {data:session} = useSession(); 
+  const {data:session,status} = useSession(); 
+ 
+ 
   return (
-    <div className='h-screen w-full flex justify-center items-center flex-col gap-4'>
+  <div className='h-screen w-full flex justify-center items-center flex-col gap-4'>
+  {status === "loading" ? "Loading session":   <div >
       {session ? <div className='space-y-3'> 
         <Image src={session?.user?.image as string} height={100} width={100} alt='avatar' className='border p-2 ring-1 border-sky-500 rounded-full'/>
         <h4 className='font-semibold text-sm text-center'>{session?.user?.name}</h4>
       </div>: <SignIn />}
-    </div>
+    </div>}
+  </div>
   )
 }
 
