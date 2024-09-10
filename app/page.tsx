4259c91ed29fdae9fe12b6ff7e8891dcb58d9374
aziz-codes <azizisoffline@gmail.com>
2024-09-10 22:46:16 +0500

    center name

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ const HomePage = () => {
   const {data:session} = useSession(); 
   return (
     <div className='h-screen w-full flex justify-center items-center flex-col gap-4'>
-      {session ? <div>
+      {session ? <div className='space-y-3'> 
         <Image src={session?.user?.image as string} height={100} width={100} alt='avatar' className='border p-2 ring-1 border-sky-500 rounded-full'/>
-        <h4 className='font-semibold text-sm'>{session?.user?.name}</h4>
+        <h4 className='font-semibold text-sm text-center'>{session?.user?.name}</h4>
       </div>: <SignIn />}
     </div>
   )
